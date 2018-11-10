c4a22106d2d2456719ac3fde0c646193f5ad8a4f
Dan Prince <danpprince@gmail.com>
2018-11-10 18:46:44 -0500

    Maybe build status will be shown on Github

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -27,6 +27,7 @@ import java.util.Date;
 public class Robot extends TimedRobot 
 {
 	private static final String ROBOT_NAME = "2018 PowerUp (ECLIPSE)-CMD BASED";
+
 	
 	// create instance of singelton Subsystems
 	private Dashboard _dashboard = Dashboard.getInstance();
